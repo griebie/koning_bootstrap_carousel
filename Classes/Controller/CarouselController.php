cc8b620b3aa01e3f40aab02061eb525622348914
Jeroen van Vierzen <jeroen@tellix.nl>
2016-11-29 14:00:39 +0100

    Added option to allow template overwrite within plugin flexform.

--- Classes/Controller/CarouselController.php
+++ Classes/Controller/CarouselController.php
@@ -22,6 +22,13 @@ class CarouselController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionControl
     public function showAction()
     {
         $currentId = (int) $this->configurationManager->getContentObject()->getFieldVal('uid');
+
+        if (!empty($this->settings['data']['template'])) {
+            $this->view = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Fluid\View\StandaloneView::class);
+            $this->view->setLayoutRootPath('EXT:koning_bootstrap_carousel/Resources/Private/Layouts/');
+            $this->view->setTemplatePathAndFilename($this->settings['data']['template']);
+        }
+
         $this->view->assignMultiple(array(
             'identifier' => 'koning-bootstrap-carousel-id-' . $currentId,
             'slides' => $this->slideRepository->findByContentId($currentId),
